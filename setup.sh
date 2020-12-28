d9e3817bb284239fea65b42aa0d8fefadb0e4ea9
Michael Hertsens <michael28072000@outlook.com>
2020-12-28 12:59:31 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ git pull;
 
 cp $1 ./docker-compose.yml;
 cp $2 ./Jwt.toml;
-cp ./Jwt.toml ./backend/Jwt.toml;
+cp ./Jwt.toml ./backend/xam-xam-bis-web/Jwt.toml;
+cp ./Jwt.toml ./backend/xam-xam-id-web/Jwt.toml;
 
 chmod +x ./build.sh;
 
