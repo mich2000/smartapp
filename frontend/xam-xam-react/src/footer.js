b07a96cf8fd3a28039b3b3188aff1baf8ad1dddb
mich2000 <michael28072000@outlook.com>
2021-01-07 21:45:00 +0100

    [0]

--- frontend/xam-xam-react/src/footer.js
+++ frontend/xam-xam-react/src/footer.js
@@ -4,8 +4,7 @@ import React from 'react';
 export function Footer(){
     return (
         <div className="sticky-footer">
-            <p>Author web app: Michael Hertsens</p>
-            <a href="https://github.com/mich2000">Github account author</a>
+            <a href="https://github.com/mich2000">Author web app: Michael Hertsens</a>
         </div>
     );
 }
