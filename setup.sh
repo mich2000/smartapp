5e1a5ae6944b2d6b4c18138a5521a9b91826309e
Michael Hertsens <michael28072000@outlook.com>
2021-01-11 11:20:37 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ cp $1 ./docker-compose.yml;
 cp $2 ./Jwt.toml;
 cp ./Jwt.toml ./backend/Jwt.toml;
 
-docker system prune;
+docker system prune -f;
 
 bash ./build.sh ./Jwt.toml && docker-compose down && docker-compose up -d;
