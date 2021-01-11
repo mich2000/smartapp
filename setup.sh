a63eea6ccebee2c0a088accc4160028ca605eac8
Michael Hertsens <michael28072000@outlook.com>
2021-01-11 11:54:28 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@ cp $1 ./docker-compose.yml;
 cp $2 ./Jwt.toml;
 cp ./Jwt.toml ./backend/Jwt.toml;
 
-docker system prune -f;
+docker-compose down;
+
+docker rmi xam-id;
+docker rmi xam-bis;
 
 bash ./build.sh ./Jwt.toml && docker-compose down && docker-compose up -d;
