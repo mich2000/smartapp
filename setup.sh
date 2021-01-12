9d8341c548b172e17ec267d53e51f6f14f9c5763
Michael Hertsens <michael28072000@outlook.com>
2021-01-12 13:35:38 +0100

    update

--- setup.sh
+++ setup.sh
@@ -4,12 +4,8 @@ cp ./Jwt.toml ./backend/Jwt.toml;
 
 docker-compose down;
 
-docker rmi xam-id:latest;
-docker rmi xam-bis:latest;
-docker rmi xam-web:latest;
-
-docker rmi xam-id:staging;
-docker rmi xam-bis:staging;
-docker rmi xam-web:staging;
+docker rmi xam-id;
+docker rmi xam-bis;
+docker rmi xam-web;
 
 bash ./build.sh ./Jwt.toml && docker-compose down && docker-compose up -d;
